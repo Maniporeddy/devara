97a37a498f693119c80b9db169fb646a9777115b
Maniporeddy <poreddymanikanta555@gmail.com>
2024-09-30 12:54:29 +0530

    Update hy.sh

--- hy.sh
+++ hy.sh
@@ -1,3 +1,5 @@
 hlo
 nbjkb,mn mnm
 https://github.com/Cohesity2022/certified-kubernetes-administrator/tree/main/Cluster%20Setup
+kubeadm join 192.168.212.3:6443 --token idre5v.aprx1n3d4uki6hex \
+	--discovery-token-ca-cert-hash sha256:028b7cbed49a855415b3a4d2dc1834bc27bf61acc66d3cc5bdffa61aa7ac9608 
